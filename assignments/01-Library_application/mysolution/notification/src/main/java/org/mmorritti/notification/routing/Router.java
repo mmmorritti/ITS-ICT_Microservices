575ce9deeea88f0b926dfc5b24ec77ea3fe0598d
mmorritti <mattia.morriti@gmail.com>
2021-01-13 21:46:28 +0100

    20210113

---
+++ assignments/01-Library_application/mysolution/notification/src/main/java/org/mmorritti/notification/routing/Router.java
@@ -0,0 +1,23 @@
+package org.mmorritti.notification.routing;
+
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Router extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+        // Kafka Consumer
+        from("kafka:{{kafka.sms.topic}}?brokers={{kafka.broker.urls}}")
+                .log("Message received from Kafka : ${body}")
+                .log("    on the topic ${headers[kafka.TOPIC]}")
+                .log("    on the partition ${headers[kafka.PARTITION]}")
+                .log("    with the offset ${headers[kafka.OFFSET]}")
+                .log("    with the key ${headers[kafka.KEY]}")
+                .to("log:?level=INFO&showBody=true");
+
+    }
+}
